d73bb679b4dca77066e674fc72a3232db1045174
M-source-JWP <mikepennefather1@gmail.com>
2021-09-18 21:27:28 -0700

    Started on enemies and slimes

--- Assets/Scripts/Enemies.cs
+++ Assets/Scripts/Enemies.cs
@@ -2,23 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemies
+abstract public class Enemies
     : MonoBehaviour
 {
-    int moveSpeed;
-    int attackDamage;
-    int lifePoints;
-    float attackRadius;
+    public float timer;
+    public int moveSpeed;
+    public int attackDamage;
+    public int healthPoints;
+    public float attackRadius;
+    public int SECONDS_FOR_ENEMY_UPDATE;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = timer + 0.01f;
+        if (timer >= SECONDS_FOR_ENEMY_UPDATE)
+        {
+            MoveEnemy();
+            AttackPlayer();
+            TakeDamage(0);
+            Debug.Log("Enemy ticked!");
+            timer = 0;
+        }
+    }
+
+    abstract public void MoveEnemy();
+
+    abstract public void AttackPlayer();
+ 
+
+    void TakeDamage(int dmg) //call this after player hits enemy
+    {
+        if ((this.healthPoints - dmg) > 0)
+        {
+            this.healthPoints = this.healthPoints - dmg;
+            //TODO play some sort of sound or anim to signify damage taken
+        }
+        else
+        {
+            this.Die();
+        }
+    }
+
+
+    void Die() //enemy dies
+    {
+        //delete enemy, play sound
+    }
+
+    public void setMoveSpeed(int speed)
+    {
+        moveSpeed = speed;
+    }
+
+    public void setAttackDamage(int attdmg)
+    {
+        attackDamage = attdmg;
+    }
+
+    public void setHealthPoints(int lp)
+    {
+        healthPoints = lp;
+    }
+
+    public int getMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public int getAttackDamage()
+    {
+        return attackDamage;
+    }
+
+    public int getHealthPoints()
+    {
+        return healthPoints;
     }
 }
---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Slime : Enemies
+    {
+        public override void AttackPlayer()
+        {
+            // if (distancetoplayer<=0) and las
+            // deal this.attackDamage to player
+            //
+        }
+
+        public override void MoveEnemy()
+        {
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            this.SECONDS_FOR_ENEMY_UPDATE = 3;
+            this.attackDamage = 4;
+            this.moveSpeed = 1;
+            this.attackRadius = 0.5f;
+            this.healthPoints = 8;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
